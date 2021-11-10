ce763b95de8d19e3841b0885db37310e4b93534d
temirlanmur <76197653+temirlanmur@users.noreply.github.com>
2021-11-10 15:04:39 +0600

    Plug-in PostgreSQL db, add store app
    Created db and user (locally), configured settings.py.
    Added store app, which contains Book model.
    Added BookViewSet with DRF.

--- books_project/settings.py
+++ books_project/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    'store.apps.StoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,8 +80,12 @@ WSGI_APPLICATION = 'books_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'books_db',
+        'USER': 'books_user',
+        'PASSWORD': os.getenv('DB_USER_PASS'),
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
